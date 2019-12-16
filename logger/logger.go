12f40e8617af340aabe9db5af5b75ffca4e33c0c
Richard Bertozzo <richardjrbertozzo@gmail.com>
2019-12-16 15:46:20 -0300

    Create funcs for logger with zap

--- logger/logger.go
+++ logger/logger.go
@@ -1,9 +1,51 @@
 package logger
 
-import (
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
+// A global variable so that log functions can be directly accessed
+var log Logger
+
+const (
+	// DEBUG has verbose message
+	DEBUG = "debug"
+	// INFO is default log level
+	INFO = "info"
+	// WARN is for logging messages about possible issues
+	WARN = "warn"
+	// ERROR is for logging errors
+	ERROR = "error"
+	// FATAL is for logging fatal messages. The sytem shutsdown after logging the message.
+	FATAL = "fatal"
 )
+
+//Logger is our contract for the logger
+type Logger interface {
+	Debug(msg string)
+	Debugw(msg string, keysAndValues ...interface{})
+	Debugf(template string, args ...interface{})
+
+	Info(msg string)
+	Infow(msg string, keysAndValues ...interface{})
+	Infof(template string, args ...interface{})
+
+	Warn(msg string)
+	Warnw(msg string, keysAndValues ...interface{})
+	Warnf(template string, args ...interface{})
+
+	Error(msg string)
+	Errorw(msg string, keysAndValues ...interface{})
+	Errorf(template string, args ...interface{})
+
+	Fatal(msg string)
+	Fatalw(msg string, keysAndValues ...interface{})
+	Fatalf(template string, args ...interface{})
+
+	Panic(msg string)
+	Panicw(msg string, keysAndValues ...interface{})
+	Panicf(template string, args ...interface{})
+}
+
+func init() {
+	log, _ = newZapLogger(Configuration{})
+}
 
 // BaseFields represents the base fields for create the basic fields of logger.
 type BaseFields struct {
@@ -12,34 +54,22 @@ type BaseFields struct {
 	CodeVersion string
 }
 
-// NewLogger Create a logger with prod or dev config.
-// isJSON for JSON output and production config and encoder
-// serviceName for set the default field service name in logger
-func NewLogger(isJSON bool, baseFields BaseFields) *zap.SugaredLogger {
-	var config zap.Config
+// Configuration stores the config for the logger
+// For some loggers there can only be one level across writers, for such the level of Console is picked by default
+type Configuration struct {
+	IsJSON     bool
+	Level      string
+	BaseFields BaseFields
+}
 
-	if isJSON {
-		encoderConfig := zap.NewProductionEncoderConfig()
-		encoderConfig.MessageKey = "message"
-		encoderConfig.TimeKey = "time"
-		encoderConfig.StacktraceKey = "stack"
-		encoderConfig.EncodeTime = zapcore.RFC3339TimeEncoder
-
-		config = zap.NewProductionConfig()
-		config.InitialFields = getBaseFields(baseFields)
-		config.EncoderConfig = encoderConfig
-	} else {
-		config = zap.NewDevelopmentConfig()
-		config.EncoderConfig = zap.NewDevelopmentEncoderConfig()
-	}
-
-	logger, err := config.Build()
+//NewLogger returns an instance of logger
+func NewLogger(config Configuration) error {
+	logger, err := newZapLogger(config)
 	if err != nil {
-		panic(err)
+		return err
 	}
-	defer logger.Sync()
-
-	return logger.Sugar()
+	log = logger
+	return nil
 }
 
 // getBaseFields returns the map of basic fields that should appear in every log output.
@@ -56,4 +86,106 @@ func getBaseFields(baseFields BaseFields) map[string]interface{} {
 	}
 
 	return initFields
+}
+
+// Debug log a debug message.
+func Debug(msg string) {
+	log.Debug(msg)
+}
+
+// Debugw logs a message with some additional context,
+// With key and values, example: log.Debugw("message", "url", url, "attempt", 3)
+// Keys in key-value pairs should be strings.
+func Debugw(msg string, keysAndValues ...interface{}) {
+	log.Debugw(msg, keysAndValues...)
+}
+
+// Debugf uses fmt.Sprintf to log a templated message.
+func Debugf(template string, args ...interface{}) {
+	log.Debugf(template, args...)
+}
+
+// Info log a info message.
+func Info(msg string) {
+	log.Info(msg)
+}
+
+// Infow logs a message with some additional context,
+// With key and values, example: log.Infow("message", "url", url, "attempt", 3)
+// Keys in key-value pairs should be strings.
+func Infow(msg string, keysAndValues ...interface{}) {
+	log.Infow(msg, keysAndValues...)
+}
+
+// Infof uses fmt.Sprintf to log a templated message.
+func Infof(template string, args ...interface{}) {
+	log.Infof(template, args...)
+}
+
+// Warn log a warn message.
+func Warn(msg string) {
+	log.Warn(msg)
+}
+
+// Warnw logs a message with some additional context,
+// With key and values, example: log.Warnw("message", "url", url, "attempt", 3)
+// Keys in key-value pairs should be strings.
+func Warnw(msg string, keysAndValues ...interface{}) {
+	log.Warnw(msg, keysAndValues...)
+}
+
+// Warnf uses fmt.Sprintf to log a templated message.
+func Warnf(template string, args ...interface{}) {
+	log.Warnf(template, args...)
+}
+
+// Error log a error message.
+func Error(msg string) {
+	log.Error(msg)
+}
+
+// Errorw logs a message with some additional context,
+// With key and values, example: log.Errorw("message", "url", url, "attempt", 3)
+// Keys in key-value pairs should be strings.
+func Errorw(msg string, keysAndValues ...interface{}) {
+	log.Errorw(msg, keysAndValues...)
+}
+
+// Errorf uses fmt.Sprintf to log a templated message.
+func Errorf(template string, args ...interface{}) {
+	log.Errorf(template, args...)
+}
+
+// Fatal log a fatal message.
+func Fatal(msg string) {
+	log.Fatal(msg)
+}
+
+// Fatalw logs a message with some additional context,
+// With key and values, example: log.Fatalw("message", "url", url, "attempt", 3)
+// Keys in key-value pairs should be strings.
+func Fatalw(msg string, keysAndValues ...interface{}) {
+	log.Fatalw(msg, keysAndValues...)
+}
+
+// Fatalf uses fmt.Sprintf to log a templated message.
+func Fatalf(template string, args ...interface{}) {
+	log.Fatalf(template, args...)
+}
+
+// Panic log a panic message.
+func Panic(msg string) {
+	log.Panic(msg)
+}
+
+// Panicw logs a message with some additional context,
+// With key and values, example: log.Panicw("message", "url", url, "attempt", 3)
+// Keys in key-value pairs should be strings.
+func Panicw(msg string, keysAndValues ...interface{}) {
+	log.Panicw(msg, keysAndValues...)
+}
+
+// Panicf uses fmt.Sprintf to log a templated message.
+func Panicf(template string, args ...interface{}) {
+	log.Panicf(template, args...)
 }
---
+++ logger/zap.go
@@ -0,0 +1,132 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type zapLogger struct {
+	sugaredLogger *zap.SugaredLogger
+}
+
+func getZapLevel(level string) zapcore.Level {
+	switch level {
+	case INFO:
+		return zapcore.InfoLevel
+	case WARN:
+		return zapcore.WarnLevel
+	case DEBUG:
+		return zapcore.DebugLevel
+	case ERROR:
+		return zapcore.ErrorLevel
+	case FATAL:
+		return zapcore.FatalLevel
+	default:
+		return zapcore.InfoLevel
+	}
+}
+
+// newZapLogger Create a logger with prod or dev config.
+// isJSON for JSON output and production config and encoder
+// serviceName for set the default field service name in logger
+func newZapLogger(config Configuration) (Logger, error) {
+	var c zap.Config
+
+	if config.IsJSON {
+		encoderConfig := zap.NewProductionEncoderConfig()
+		encoderConfig.MessageKey = "message"
+		encoderConfig.TimeKey = "time"
+		encoderConfig.StacktraceKey = "stack"
+		encoderConfig.EncodeTime = zapcore.RFC3339TimeEncoder
+
+		c = zap.NewProductionConfig()
+		c.InitialFields = getBaseFields(config.BaseFields)
+		c.EncoderConfig = encoderConfig
+		c.Level = zap.NewAtomicLevelAt(getZapLevel(config.Level))
+	} else {
+		c = zap.NewDevelopmentConfig()
+		c.EncoderConfig = zap.NewDevelopmentEncoderConfig()
+	}
+
+	logger, err := c.Build()
+	if err != nil {
+		return nil, err
+	}
+	defer logger.Sync()
+
+	return &zapLogger{
+		sugaredLogger: logger.Sugar(),
+	}, nil
+}
+
+func (l *zapLogger) Debug(msg string) {
+	l.sugaredLogger.Debug(msg)
+}
+
+func (l *zapLogger) Debugw(msg string, keysAndValues ...interface{}) {
+	l.sugaredLogger.Debugw(msg, keysAndValues...)
+}
+
+func (l *zapLogger) Debugf(template string, args ...interface{}) {
+	l.sugaredLogger.Debugf(template, args...)
+}
+
+func (l *zapLogger) Info(msg string) {
+	l.sugaredLogger.Info(msg)
+}
+
+func (l *zapLogger) Infow(msg string, keysAndValues ...interface{}) {
+	l.sugaredLogger.Infow(msg, keysAndValues...)
+}
+
+func (l *zapLogger) Infof(template string, args ...interface{}) {
+	l.sugaredLogger.Infof(template, args...)
+}
+
+func (l *zapLogger) Warn(msg string) {
+	l.sugaredLogger.Warn(msg)
+}
+
+func (l *zapLogger) Warnw(msg string, keysAndValues ...interface{}) {
+	l.sugaredLogger.Warnw(msg, keysAndValues...)
+}
+
+func (l *zapLogger) Warnf(template string, args ...interface{}) {
+	l.sugaredLogger.Warnf(template, args...)
+}
+
+func (l *zapLogger) Error(msg string) {
+	l.sugaredLogger.Error(msg)
+}
+
+func (l *zapLogger) Errorw(msg string, keysAndValues ...interface{}) {
+	l.sugaredLogger.Errorw(msg, keysAndValues...)
+}
+
+func (l *zapLogger) Errorf(template string, args ...interface{}) {
+	l.sugaredLogger.Errorf(template, args...)
+}
+
+func (l *zapLogger) Fatal(msg string) {
+	l.sugaredLogger.Fatal(msg)
+}
+
+func (l *zapLogger) Fatalw(msg string, keysAndValues ...interface{}) {
+	l.sugaredLogger.Fatalw(msg, keysAndValues...)
+}
+
+func (l *zapLogger) Fatalf(template string, args ...interface{}) {
+	l.sugaredLogger.Fatalf(template, args...)
+}
+
+func (l *zapLogger) Panic(msg string) {
+	l.sugaredLogger.Panic(msg)
+}
+
+func (l *zapLogger) Panicw(msg string, keysAndValues ...interface{}) {
+	l.sugaredLogger.Panicw(msg, keysAndValues...)
+}
+
+func (l *zapLogger) Panicf(template string, args ...interface{}) {
+	l.sugaredLogger.Panicf(template, args...)
+}
